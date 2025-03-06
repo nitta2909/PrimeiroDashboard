15ca36d7e194062aeca06fbb19b952ba71849a32
Mauricio Nitta <nitta2909@gmail.com>
2025-03-07 04:15:44 +0900

    Configurado HashRouter para resolver problemas de navegação no GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
